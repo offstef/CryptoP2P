66e65e9dc407eb76313e65f194c4f8b76c25aafb
skibo <off.a@aol.com>
2024-04-24 20:42:28 +0200

    added console color fonts for banners, romania type jijiji

--- src/resources/Banners.java
+++ src/resources/Banners.java
@@ -1,21 +1,21 @@
 package resources;
 
 public class Banners {
-	
+	public static final String COLOR_RESET = "\u001B[0m";
 	public void title () {
-		System.err.println("  @@@@@@@ @@@@@@@  @@@ @@@ @@@@@@@  @@@@@@@  @@@@@@    @@@@@@@   @@@@@@  @@@@@@@ ");
-        System.err.println(" !@@      @@!  @@@ @@! !@@ @@!  @@@   @@!   @@!  @@@   @@!  @@@ @@   @@@ @@!  @@@");
-        System.err.println(" !@!      @!@!!@!   !@!@!  @!@@!@!    @!!   @!@  !@!   @!@@!@!    .!!@!  @!@@!@! ");
-        System.err.println(" :!!      !!: :!!    !!:   !!:        !!:   !!:  !!!   !!:       !!:     !!:     ");
-        System.err.println("  :: :: :  :   : :   .:     :          :     : :. :    :       :.:: :::  :      ");
-        System.err.println("                                                                                ");
+		System.out.println("\u001B[34m  @@@@@@@ @@@@@@@  @@@ @@@ @@@@@@@  @@@@@@@  @@@@@@    @@@@@@@   @@@@@@  @@@@@@@ ");
+        System.out.println("\u001B[33m !@@      @@!  @@@ @@! !@@ @@!  @@@   @@!   @@!  @@@   @@!  @@@ @@   @@@ @@!  @@@");
+        System.out.println("\u001B[33m !@!      @!@!!@!   !@!@!  @!@@!@!    @!!   @!@  !@!   @!@@!@!    .!!@!  @!@@!@! ");
+        System.out.println("\u001B[31m :!!      !!: :!!    !!:   !!:        !!:   !!:  !!!   !!:       !!:     !!:     ");
+        System.out.println("\u001B[31m  :: :: :  :   : :   .:     :          :     : :. :    :       :.:: :::  :      "+ COLOR_RESET);
+        System.out.println();
 	}
 	
 	public void end() {
-		System.err.println(" _____ _____ ____    ");
-        System.err.println("|   __|   | |    \\   ");
-        System.err.println("|   __| | | |  |  |_ ");
-        System.err.println("|_____|_|___|____/|_|");
+		System.err.println("\u001B[34m _____ _____ ____    ");
+        System.err.println("\u001B[34m|   __|   | |    \\   ");
+        System.err.println("\u001B[33m|   __| | | |  |  |_ ");
+        System.err.println("\u001B[31m|_____|_|___|____/|_|"+ COLOR_RESET);
 	}
 	
 }
